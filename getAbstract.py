f21596c1e9e4ef0fb2eeae7611009ce97e39d6b2
Adam Tan <suwekforu@gmail.com>
2023-02-26 11:49:26 +0800

    using summarizer on main pdf now

--- getAbstract.py
+++ getAbstract.py
@@ -4,7 +4,7 @@ from lxml import etree
 
 doi = "10.2196/40201" # Replace with the DOI of the research article you want to get the abstract for
 
-url = f"http://api.crossref.org/works/{doi}"
+#url = f"http://api.crossref.org/works/{doi}"
 def abst(url):
     response = requests.get(url)
     data = response.json().get('message').get('abstract')
